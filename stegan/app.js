aeed86a51fd84ee300817c4b3ea4801c2f707c21
Artem Shevchenko <shevchenkoaa@sgu.ru>
2016-12-02 11:27:19 +0300

    Rewrited on ES6 standard

--- stegan/app.js
+++ stegan/app.js
@@ -1,22 +1,24 @@
 'use strict'
-const Steganography = require("./lib/stegan");
+const Steganography = require("./lib/steganography");
 const fs = require("fs");
 
 var ciph = new Steganography();
-var hide_text = fs.readFileSync(process.argv[2]).toString(),
-    container_text = fs.readFileSync(process.argv[3]).toString();
+var hide_text = fs.readFileSync(process.argv[2]).toString();
 
 ciph.hide(hide_text);
 
-var enc = ciph.encode(container_text);
-fs.writeFile("enc.txt", enc, function(err){
-  if(err){
-    return console.err(err);
-  }
+fs.readFile(process.argv[3], "utf8", function(err, data){
+  var enc = ciph.encode(data);
+
+  fs.writeFile("enc.txt", enc, function(err){
+    if(err){
+      return console.err(err);
+    }
+  });
 });
 
-fs.readFile("enc.txt", "utf8", function(err, data){
-  var dec = ciph.decode(data);
+fs.readFile("enc.txt", "utf8", function(err, enc_data){
+  var dec = ciph.decode(enc_data);
 
   fs.writeFile("dec.txt", dec, function(err){
     if(err){
