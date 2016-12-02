7d9a741fe15be35c70c0cdab84f43e3d35b2a5ca
Artem Shevchenko <shevchenkoaa@sgu.ru>
2016-12-02 14:08:03 +0300

    Added CLI options

--- stegan/app.js
+++ stegan/app.js
@@ -7,22 +7,23 @@ var hide_text = fs.readFileSync(process.argv[2]).toString();
 
 ciph.hide(hide_text);
 
-fs.readFile(process.argv[3], "utf8", function(err, data){
-  var enc = ciph.encode(data);
+if(process.argv[3] == "--encode"){
+  fs.readFile(process.argv[4], "utf8", function(err, data){
+    var enc = ciph.encode(data);
 
-  fs.writeFile("enc.txt", enc, function(err){
-    if(err){
-      return console.err(err);
-    }
+    fs.writeFile("enc.txt", enc, function(err){
+      if(err){
+        return console.err(err);
+      }
+    });
   });
-});
-
-fs.readFile("enc.txt", "utf8", function(err, enc_data){
-  var dec = ciph.decode(enc_data);
-
-  fs.writeFile("dec.txt", dec, function(err){
-    if(err){
-      return console.err(err);
-    }
+}else if(process.argv[3] == "--decode") {
+  fs.readFile(process.argv[4], "utf8", function(err, enc_data){
+    var dec = ciph.decode(enc_data);
+    fs.writeFile("dec.txt", dec, function(err){
+      if(err){
+        return console.err(err);
+      }
+    });
   });
-});
+}
